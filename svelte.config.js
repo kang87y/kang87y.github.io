379876416a53b8a3c8064d5bd3fb19fa8556631f
kang87y <kang87y.code@gmail.com>
2024-12-22 16:30:31 +0900

    feat: Update Node.js version and switch to Vercel adapter in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,8 @@ import { mdsvex } from 'mdsvex';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug';
 
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,7 +19,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: vercel({
+			runtime: 'nodejs20.x'
+		})
 	}
 };
 
