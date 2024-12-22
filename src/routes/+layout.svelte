1807812e9052ae309e6752fad64d2b9c351dfb7a
kang87y <kang87y.code@gmail.com>
2024-12-22 15:58:49 +0900

    feat: Update syntax highlighting integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,21 +5,4 @@
 
 <svelte:head>
 	<link rel="stylesheet" href="/src/lib/styles.css" />
-	<link
-		rel="stylesheet"
-		href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark-dimmed.min.css"
-	/><script
-		src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js"
-	></script><script
-		src="//cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.7.0/highlightjs-line-numbers.min.js"
-	></script><script>
-		hljs.initHighlightingOnLoad();
-	</script><script>
-		hljs.initLineNumbersOnLoad();
-		$(document).ready(function () {
-			$('code.hljs').each(function (i, block) {
-				hljs.lineNumbersBlock(block);
-			});
-		});
-	</script>
 </svelte:head>
